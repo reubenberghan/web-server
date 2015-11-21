67d2e7377578bcc34837ac065bf9ffe528bec04e
Reuben Berghan <reuben.berghan@gmail.com>
2015-11-21 15:25:56 +1100

    Initialise repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		var date = new Date().toString();
+		console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+// app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+// app.get('/', function(req, res) {
+// 	res.send('Hello Express!');
+// });
+
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+	res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function() { console.log('Express server started on port: ' + PORT); });
